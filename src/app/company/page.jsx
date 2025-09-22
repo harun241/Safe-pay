fe6947c3672587ea6dab4667ae33e55911404a68
harun241 <omdharun6@gmail.com>
2025-09-22 22:11:36 +0600

    slash

--- src/app/company/page.jsx
+++ src/app/company/page.jsx
@@ -43,7 +43,7 @@ const OurCompanySection = () => {
   return (
     <section className="bg-gradient-to-r from-gray-900 via-gray-800 to-gray-900 text-white py-16 px-6">
       <div className="max-w-7xl mx-auto">
-        <h2 className="text-3xl font-bold mb-12 text-center">Our Company</h2>
+        <h2 className="text-3xl font-bold mb-12 text-center">Our -Company</h2>
         <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
           {companySections.map((item, idx) => (
             item.href ? (
