a9b33fe0142d36b520699ebc524db53da3fc3680
Muksitul Islam <muksitul15-3962@diu.edu.bd>
2025-10-01 23:11:43 +0600

    fix the suspance issus on the video-call page

--- src/app/(base)/video-call/page.js
+++ src/app/(base)/video-call/page.js
@@ -4,23 +4,21 @@ import { useSearchParams } from "next/navigation";
 import VideoChat from "../Components/VideoChat";
 import { Suspense } from "react";
 
-export default function VideoCall() {
+export default function VideoCallPageWrapper() {
+    return (
+        <Suspense fallback={<div>Loading Video...</div>}>
+            <VideoCallPage />
+        </Suspense>
+    );
+}
+
+function VideoCallPage() {
     const searchParams = useSearchParams();
     const roomId = searchParams.get("room") || `room-${Math.floor(Math.random() * 10000)}`;
 
-    //   video chat
     return (
         <div className="w-screen h-screen bg-gray-100 dark:bg-gray-900 flex items-center justify-center p-4">
             <VideoChat roomId={roomId} />
         </div>
     );
-}
-
-
-export function VideoCallPage() {
-    return (
-        <Suspense fallback={<div>Loading Video......</div>}>
-            <VideoCall></VideoCall>
-        </Suspense>
-    )
 }
