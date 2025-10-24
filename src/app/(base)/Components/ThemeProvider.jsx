b078781635ec061a18568554700b321cd2a6df38
harun241 <omdharun6@gmail.com>
2025-10-24 10:12:40 +0600

    little conflict in theme provider

--- src/app/(base)/Components/ThemeProvider.jsx
+++ src/app/(base)/Components/ThemeProvider.jsx
@@ -1,18 +1,13 @@
-'use client';
+"use client";
 
-import { ThemeProvider as NextThemesProvider } from 'next-themes';
+import { ThemeProvider as NextThemesProvider } from "next-themes";
 
 export function ThemeProvider({ children, ...props }) {
   return (
     <NextThemesProvider
       attribute="class"
-<<<<<<< HEAD
-      defaultTheme="dark"       
-      enableSystem={true}      
-=======
       defaultTheme="dark"
-      enableSystem
->>>>>>> 0bbccc81bc77cb333eb46689c53299634e2992b9
+      enableSystem={true}
       disableTransitionOnChange
       {...props}
     >
