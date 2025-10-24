4973d9db412fbfeb1e5d1490c112d35310bc2d38
Muksitul Islam <muksitul15-3962@diu.edu.bd>
2025-10-24 15:22:52 +0600

    added suspance in the success payment

--- src/app/layout.js
+++ src/app/layout.js
@@ -16,21 +16,22 @@ export const metadata = {
 // Root Layout
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" 
+      suppressHydrationWarning>
       <body
-      data-new-gr-c-s-check-loaded="14.1254.0"
-      data-gr-ext-installed="">
+        data-new-gr-c-s-check-loaded="14.1254.0"
+        data-gr-ext-installed="">
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
           enableSystem
           disableTransitionOnChange={false}
         >
-        <ReduxProvider>
-          <AuthProvider>
-            <main className="flex-1 ">{children}</main>
-          </AuthProvider>
-        </ReduxProvider>
+          <ReduxProvider>
+            <AuthProvider>
+              <main className="flex-1 ">{children}</main>
+            </AuthProvider>
+          </ReduxProvider>
         </ThemeProvider>
       </body>
     </html>
