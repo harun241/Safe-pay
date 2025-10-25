eb186c9785d52af4fe8b38e40052f0ffc5a27b1a
Muksitul Islam <muksitul15-3962@diu.edu.bd>
2025-10-25 23:50:33 +0600

    resolve the themeswiter issues

--- src/app/(base)/Components/Navbar.jsx
+++ src/app/(base)/Components/Navbar.jsx
@@ -299,7 +299,7 @@ export default function Navbar() {
   return (
     <>
       <header
-        className={`fixed top-0 left-0 right-0 z-50 transition-all duration-300 ${
+        className={`fixed top-0 left-0 right-0 z-1000 transition-all duration-300 ${
           isScrolled || isMobileMenuOpen
             ? theme === "dark"
               ? "bg-gray-950/80 backdrop-blur-lg border-b border-gray-700/50 shadow-lg"
